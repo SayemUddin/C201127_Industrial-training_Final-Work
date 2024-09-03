7152f956ec5026bff9109b80a75787dcf60fff86
C201127_Md. Sayem Uddin <90597295+SayemUddin@users.noreply.github.com>
2024-09-03 23:00:08 +0600

    work done

---
+++ streamlit-app/Home.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.set_page_config(page_title="News Summarization Asssignmen", layout="wide")
+
+st.header("Daily Star News Summary C201049,C201127,C193071")
+st.subheader("AI based news summarization")
+st.markdown("This app allows you to view news articles and generate summaries.")
